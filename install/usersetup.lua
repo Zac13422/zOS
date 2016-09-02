d5a3459b705ade0f477ee12921c1a9b8d50e03d7
Zac13422 <zacharie.deguilhem@gmail.com>
2016-09-02 20:40:50 +0200

    Additions in the installer

--- install/usersetup.lua
+++ install/usersetup.lua
@@ -4,13 +4,19 @@ file = fs.open("etc/usrdata/usernumber", "w")
 file.write("0")
 file.close()
 
+libinstall.setProgress(18)
+
+os.loadAPI("/api/text")
 os.loadAPI("/api/api")
-api.loadAll(0)
+libinstall.setProgress(20)
+api.loadAll(1)
+libinstall.setProgress(24)
 
 term.setCursorPos(1,3)
 write("Enter your computer name:")
 compName = read()
 os.setComputerLabel(compName)
+libinstall.setProgress(25)
 
 function ask(stra,stre,xpos,ypos,var)
 	term.setCursorPos(1,ypos)
@@ -41,5 +47,8 @@ function ask(stra,stre,xpos,ypos,var)
 end
 
 ask("Enter root password:","Invalid password",35,4,rootpass)
+libinstall.setProgress(28)
 ask("Enter your username:","Invalid username",35,5,user)
+libinstall.setProgress(31)
 ask("Enter the password for"..user..":","Invalid password",35,6,userpass)
+libinstall.setProgress(34)
